10b762bf6d7fb1fccafcd4cc922291539fb8b7d5
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-04-11 19:30:58 -0700

    setup

--- src/main/java/dev/dogie/create_foodie/CreateFoodie.java
+++ src/main/java/dev/dogie/create_foodie/CreateFoodie.java
@@ -1,4 +1,4 @@
-package com.example.modid;
+package dev.dogie.create_foodie;
 
 import com.simibubi.create.Create;
 
@@ -10,9 +10,9 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String ID = "modid";
-	public static final String NAME = "Example Mod";
+public class CreateFoodie implements ModInitializer {
+	public static final String ID = "create_foodie";
+	public static final String NAME = "Create: Foodie";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
 	@Override
--- src/main/java/dev/dogie/create_foodie/mixin/MinecraftMixin.java
+++ src/main/java/dev/dogie/create_foodie/mixin/MinecraftMixin.java
@@ -1,6 +1,6 @@
-package com.example.modid.mixin;
+package dev.dogie.create_foodie.mixin;
 
-import com.example.modid.ExampleMod;
+import dev.dogie.create_foodie.CreateFoodie;
 
 import net.minecraft.client.Minecraft;
 
@@ -15,6 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftMixin {
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void example$init(GameConfig gameConfig, CallbackInfo ci) {
-		ExampleMod.LOGGER.info("Hello from {}", ExampleMod.NAME);
+		CreateFoodie.LOGGER.info("Hello from {}", CreateFoodie.NAME);
 	}
 }
